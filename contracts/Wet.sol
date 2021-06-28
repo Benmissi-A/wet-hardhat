f6911eca469bc8688a7a06f037e725ab02b61229
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-28 17:23:23 +0200

    wet

--- contracts/Wet.sol
+++ contracts/Wet.sol
@@ -2,8 +2,30 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Wet is ERC20 {
-    constructor(address owner_, uint256 initialSupply) ERC20("Wrapped Ether", "WET") {
+    using Address for address payable;
+    uint256 private _balance;
+    
+    constructor(address owner_) ERC20("Wrapped Ether", "WET") {
+    }
+
+    function deposit() public payable {
+        require(msg.value > 2 ,"Wet contract : 2 wei min for a transaction");
+         _balance += msg.value;
+         _mint(msg.sender, msg.value / 2);
+    }
+    
+    function withdraw(uint256 amount) external payable {
+        require(amount > 0 ,"Wet contract : 1 wei min for a transaction");
+        transfer(address(this), (amount));
+        _balance -= amount*2;
+        _burn(address(this), (amount)) ;
+        payable(msg.sender).sendValue(amount*2);
+    }
+    
+    function getBalance() public view returns(uint256){
+        return _balance;
     }
 }
